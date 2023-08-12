8427246e61583a557da81bd912412a90162d0fb4
Zeinab Abdien <suzanabdien15@gmail.com>
2023-08-12 17:43:19 +0300

    add application file

---
+++ app/src/main/java/com/iti/itiproject/MyApplication.kt
@@ -0,0 +1,32 @@
+package com.iti.itiproject
+
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.Context
+import com.google.firebase.auth.FirebaseAuth
+import com.iti.itiproject.data.datastore.DataStoreImplementation
+import javax.inject.Inject
+
+class MyApplication : Application() {
+
+    @Inject
+    lateinit var dataStore: DataStoreImplementation
+
+    companion object {
+
+        @SuppressLint("StaticFieldLeak")
+        lateinit var context: Context
+
+        @SuppressLint("StaticFieldLeak")
+        lateinit var instance: MyApplication
+            private set
+
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+        instance = this
+    }
+
+}
