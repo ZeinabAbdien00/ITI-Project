d9c1e8fd79b88e1dc5bb6984eae00d0fa90ed437
Zeinab Abdien <suzanabdien15@gmail.com>
2023-08-12 18:16:58 +0300

    add constants

---
+++ app/src/main/java/com/iti/itiproject/utils/Constants.kt
@@ -0,0 +1,9 @@
+package com.iti.itiproject.utils
+
+class Constants {
+
+    companion object {
+        const val BASE_URL = "https://corona.lmao.ninja/v2/"
+        const val SPLASH_DELAY = 300L
+    }
+}
