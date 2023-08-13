cc37cc8c08f1649f805014f004228f0e4d8e96bd
Zeinab Abdien <suzanabdien15@gmail.com>
2023-08-13 18:23:16 +0300

    add home view pager

---
+++ app/src/main/java/com/iti/itiproject/adapter/viewpager/ViewPagerAdapter.kt
@@ -0,0 +1,31 @@
+package com.iti.itiproject.adapter.viewpager
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.iti.itiproject.ui.home.all_countries.AllCountriesFragment
+import com.iti.itiproject.ui.home.search.SearchFragment
+import com.iti.itiproject.ui.home.setting.SettingFragment
+import com.iti.itiproject.ui.home.setting.advice.AdviceFragment
+import com.iti.itiproject.ui.home.setting.profile.ProfileFragment
+import com.iti.itiproject.ui.home.today.TodayFragment
+
+class ViewPagerAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
+    FragmentStateAdapter(fragmentManager, lifecycle) {
+
+    override fun getItemCount(): Int {
+        return 4
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            0 -> TodayFragment()
+            1 -> AllCountriesFragment()
+            2 -> SearchFragment()
+//            4 -> ProfileFragment()
+//            5 -> AdviceFragment()
+            else -> SettingFragment()
+        }
+    }
+}
